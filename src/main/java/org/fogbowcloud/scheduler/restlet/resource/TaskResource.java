8eb348cb68c4b3a1e7ffa32f7bf59fc421018912
gustavorag <gustavorag@localhost>
2015-11-05 17:07:34 -0300

    Implementation of get image from task service.

--- src/main/java/org/fogbowcloud/scheduler/restlet/resource/TaskResource.java
+++ src/main/java/org/fogbowcloud/scheduler/restlet/resource/TaskResource.java
@@ -1,5 +1,11 @@
 package org.fogbowcloud.scheduler.restlet.resource;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -10,6 +16,7 @@ import org.fogbowcloud.scheduler.restlet.SebalScheduleApplication;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.restlet.data.MediaType;
+import org.restlet.representation.ObjectRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Get;
@@ -21,28 +28,70 @@ public class TaskResource  extends ServerResource {
 	public Representation getEvents() throws Exception{
 		
 		String taskId = (String) getRequest().getAttributes().get("taskId");
+		String var = (String) getRequest().getAttributes().get("var");
 		
-		if (taskId != null){
-		Task task = ((SebalScheduleApplication) getApplication()).getTaskById(taskId);
-		JSONObject jsonTask = new JSONObject();
-		jsonTask.put("taskId", task.getId());
-		jsonTask.put("resultingFile", task.getMetadata(TaskImpl.METADATA_LOCAL_OUTPUT_FOLDER));
-		StringRepresentation sr = new StringRepresentation(jsonTask.toString(), MediaType.TEXT_PLAIN);
-		return sr;
-		} else {
-		List<Task> tasks = ((SebalScheduleApplication) getApplication()).getAllCompletedTasks();
-		JSONArray jasonTasks = new JSONArray();
-		for(Task e : tasks){
-			JSONObject jsonTask = new JSONObject();
-			jsonTask.put("taskId", e.getId());
-			jsonTask.put("resultingFile", e.getMetadata(TaskImpl.METADATA_LOCAL_OUTPUT_FOLDER));
-			jasonTasks.put(jsonTask);
-		}
+		
+		if (taskId != null) {
 
-		StringRepresentation sr = new StringRepresentation(jasonTasks.toString(), MediaType.TEXT_PLAIN);
-		return sr;
+			Task task = ((SebalScheduleApplication) getApplication()).getTaskById(taskId);
+
+			if (var != null) {
+				//TODO validate if task is completed and has image related to var
+				//Mount fileName.
+				String fileName = "";
+				return this.getImagemAsRepresentation(fileName);
+			}
+
+			JSONObject jsonTask = new JSONObject();
+			jsonTask.put("taskId", task.getId());
+			jsonTask.put("resultingFile", task.getMetadata(TaskImpl.METADATA_LOCAL_OUTPUT_FOLDER));
+			StringRepresentation sr = new StringRepresentation(jsonTask.toString(), MediaType.TEXT_PLAIN);
+			return sr;
+		} else {
+			List<Task> tasks = ((SebalScheduleApplication) getApplication()).getAllCompletedTasks();
+			JSONArray jasonTasks = new JSONArray();
+			for (Task e : tasks) {
+				JSONObject jsonTask = new JSONObject();
+				jsonTask.put("taskId", e.getId());
+				jsonTask.put("resultingFile", e.getMetadata(TaskImpl.METADATA_LOCAL_OUTPUT_FOLDER));
+				jasonTasks.put(jsonTask);
+			}
+
+			StringRepresentation sr = new StringRepresentation(jasonTasks.toString(), MediaType.TEXT_PLAIN);
+			return sr;
 		}
 	}
 	
+	
+	private Representation getImagemAsRepresentation(String fileName) throws FileNotFoundException{
+		
+		File file = new File(fileName);
+
+		FileInputStream fis = new FileInputStream(file);
+
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		byte[] buf = new byte[1024];
+		try {
+			for (int readNum; (readNum = fis.read(buf)) != -1;) {
+				// Writes to this byte array output stream
+				bos.write(buf, 0, readNum);
+			}
+		} catch (IOException ex) {
+		}
+
+		byte[] data = bos.toByteArray();
+
+		
+		ObjectRepresentation<byte[]> or=new ObjectRepresentation<byte[]>(data, MediaType.IMAGE_BMP) {
+	        @Override
+	        public void write(OutputStream os) throws IOException {
+	            super.write(os);
+	            os.write(this.getObject());
+	        }
+	    };
+
+	    return or; 
+		
+	}
 	
 }
