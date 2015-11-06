a6f3b656558d2871bda86c716aac0834ee49c451
gustavorag <gustavorag@localhost>
2015-11-06 11:44:26 -0300

    Add tasks states count for images on ImageResource return

--- src/main/java/org/fogbowcloud/scheduler/restlet/resource/ImageResource.java
+++ src/main/java/org/fogbowcloud/scheduler/restlet/resource/ImageResource.java
@@ -1,14 +1,16 @@
 package org.fogbowcloud.scheduler.restlet.resource;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.fogbowcloud.scheduler.core.model.Job.TaskState;
 import org.fogbowcloud.scheduler.core.model.Task;
 import org.fogbowcloud.scheduler.core.model.TaskImpl;
-import org.fogbowcloud.scheduler.core.model.Job.TaskState;
 import org.fogbowcloud.scheduler.restlet.SebalScheduleApplication;
 import org.fogbowcloud.sebal.ImageData;
+import org.fogbowcloud.sebal.ImageState;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.restlet.data.MediaType;
@@ -21,7 +23,6 @@ import com.google.gson.Gson;
 
 
 public class ImageResource extends ServerResource {
-	
 	
 	@Get
 	public Representation getEvents() throws Exception{
@@ -42,6 +43,24 @@ public class ImageResource extends ServerResource {
 		}
 		
 		List<ImageData> images = ((SebalScheduleApplication) getApplication()).getAllImages();
+		
+		for(ImageData image : images){
+			Map<Task, TaskState> tasks = ((SebalScheduleApplication) getApplication()).getAllTaskByImage(imageName);
+			Map<String, Integer> tasksStatesCount = new HashMap<String, Integer>();
+			for(Entry<Task, TaskState> e : tasks.entrySet()){
+				
+				Integer increment = new Integer(1);
+				
+				if(tasksStatesCount.containsKey(e.getValue().name())){
+					increment = increment+1;
+				}
+				tasksStatesCount.put(e.getValue().name(), increment);
+				
+			}
+			image.setTasksStatesCount(tasksStatesCount);
+		}
+		
+		
 		Gson gson = new Gson();
 		String jsonEvents = gson.toJson(images);
 		StringRepresentation sr = new StringRepresentation(jsonEvents, MediaType.TEXT_PLAIN);
