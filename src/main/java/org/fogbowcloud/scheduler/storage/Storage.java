f6995c9bfc3a6807e4b9a51ccf66167e3482d4fd
esdras <esdras@10.30.0.225>
2016-04-12 11:27:13 -0300

    Trying to solve a client problem

--- src/main/java/org/fogbowcloud/scheduler/storage/Storage.java
+++ src/main/java/org/fogbowcloud/scheduler/storage/Storage.java
@@ -1,13 +1,34 @@
 package org.fogbowcloud.scheduler.storage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.methods.HttpDelete;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.fogbowcloud.manager.core.plugins.IdentityPlugin;
+import org.fogbowcloud.manager.occi.model.HeaderUtils;
+import org.fogbowcloud.manager.occi.model.OCCIHeaders;
 import org.fogbowcloud.manager.occi.order.OrderAttribute;
 import org.fogbowcloud.manager.occi.order.OrderConstants;
 
@@ -29,7 +50,7 @@ public class Storage {
 	
 	private static final Logger LOGGER = Logger.getLogger(Storage.class);
 	
-	private void orderStorage() {
+	private void orderStorage() throws Exception {
 		JCommander jc = new JCommander();
 		OrderCommand order = new OrderCommand();
 		jc.addCommand("order", order);
@@ -65,7 +86,134 @@ public class Storage {
 					return;
 				}
 			}
+		} else if (parsedCommand.equals("storage")) {
+			String url = storage.url;
+			
+			String authToken = normalizeTokenFile(storage.authFile);
+			if (authToken == null) {
+				authToken = normalizeToken(storage.authToken);
+			}
+			
+			if (storage.get) {
+				if (storage.delete) {
+					jc.usage();
+					return;							
+				}	
+				
+				if (storage.storageId == null) {
+					doRequest("get", url + "/" + OrderConstants.STORAGE_TERM, authToken);
+					return;
+				} 
+				doRequest("get", url + "/" + OrderConstants.STORAGE_TERM + "/" + storage.storageId, authToken);
+			} else if (storage.delete) {
+				if (storage.get) {
+					jc.usage();
+					return;							
+				}
+
+				if (storage.storageId == null) {
+					doRequest("delete", url + "/" + OrderConstants.STORAGE_TERM, authToken);
+					return;
+				} 
+				doRequest("delete", url + "/" + OrderConstants.STORAGE_TERM + "/" + storage.storageId, authToken);
+			} else {
+				jc.usage();
+				return;				
+			}
 		}
+	}
+	
+	protected static String normalizeToken(String token) {
+		if (token == null) {
+			return null;
+		}				
+		return token.replace("\n", "");
+	}
+	
+	protected static String normalizeTokenFile(String token) {
+		if (token == null) {
+			return null;
+		}		
+		File tokenFile = new File(token);
+		if (tokenFile.exists()) {
+			try {
+				token = IOUtils.toString(new FileInputStream(tokenFile));
+			} catch (Exception e) {
+				return null;
+			}
+		} else {
+			return null;
+		}		
+		return token.replace("\n", "");
+	}	
+
+	private static void doRequest(String method, String endpoint, String authToken) throws URISyntaxException, HttpException, IOException {
+		doRequest(method, endpoint, authToken, new LinkedList<Header>());
+	}
+
+	private static void doRequest(String method, String endpoint, String authToken, 
+			List<Header> additionalHeaders) throws URISyntaxException, HttpException, IOException {
+		HttpUriRequest request = null;
+		if (method.equals("get")) {
+			request = new HttpGet(endpoint);
+		} else if (method.equals("delete")) {
+			request = new HttpDelete(endpoint);
+		} else if (method.equals("post")) {
+			request = new HttpPost(endpoint);
+		}
+		request.addHeader(OCCIHeaders.CONTENT_TYPE, OCCIHeaders.OCCI_CONTENT_TYPE);
+		if (authToken != null) {
+			request.addHeader(OCCIHeaders.X_AUTH_TOKEN, authToken);
+		}
+		for (Header header : additionalHeaders) {
+			request.addHeader(header);
+		}
+		
+		
+		/*if (client == null) {
+			client = new DefaultHttpClient();
+			HttpParams params = new BasicHttpParams();
+			params.setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+			client = new DefaultHttpClient(new ThreadSafeClientConnManager(params, client
+					.getConnectionManager().getSchemeRegistry()), params);
+		}
+
+		HttpResponse response = client.execute(request);
+
+		if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK
+				|| response.getStatusLine().getStatusCode() == HttpStatus.SC_CREATED) {
+			Header locationHeader = getLocationHeader(response.getAllHeaders());
+			if (locationHeader != null && locationHeader.getValue().contains(OrderConstants.TERM)) {
+				System.out.println(generateLocationHeaderResponse(locationHeader));
+			} else {
+				System.out.println(EntityUtils.toString(response.getEntity()));
+			}
+		} else {
+			System.out.println(response.getStatusLine().toString());
+		}*/
+	}
+	
+	protected static Header getLocationHeader(Header[] headers) {
+		Header locationHeader = null;
+		for (Header header : headers) {	
+			if (header.getName().equals("Location")) {
+				locationHeader = header;
+			}
+		}
+		return locationHeader;
+	}
+	
+	protected static String generateLocationHeaderResponse(Header header) {
+		String[] locations = header.getValue().split(",");
+		String response = "";
+		for (String location : locations) {
+			response += HeaderUtils.X_OCCI_LOCATION_PREFIX + location + "\n";
+		}
+		return response.trim();
+	}
+
+	public static void setClient(HttpClient client) {
+		Storage.client = client;
 	}
 
 	private static class Command {
