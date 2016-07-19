b36aca8c05094df961a933a28fde84ae938586fc
Thiago Manel <thiago.manel@gmail.com>
2016-07-19 11:53:38 -0300

    clean BootstrapMain code

--- src/main/java/org/fogbowcloud/sebal/engine/sebal/bootstrap/BootstrapMain.java
+++ src/main/java/org/fogbowcloud/sebal/engine/sebal/bootstrap/BootstrapMain.java
@@ -9,69 +9,75 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.log4j.Logger;
 import org.fogbowcloud.sebal.engine.sebal.ImageData;
 import org.fogbowcloud.sebal.engine.sebal.ImageState;
 
 public class BootstrapMain {
 
-	static String SELECT_ALL_IMAGES_SQL = "SELECT * FROM nasa_images ORDER BY priority, image_name";
+    private static final Logger LOGGER = Logger.getLogger(BootstrapMain.class);
 
-	private static BasicDataSource connectionPool;
+    private static final String DATASTORE_USERNAME = "datastore_username";
+    private static final String DATASTORE_PASSWORD = "datastore_password";
+    private static final String DATASTORE_DRIVER = "datastore_driver";
+    private static final String DATASTORE_URL_PREFIX = "datastore_url_prefix";
 
-	public static void main(String[] args) throws IOException, SQLException {
-		final Properties properties = new Properties();
-		FileInputStream input = new FileInputStream(args[0]);
-		properties.load(input);
-		
-		String sqlIP = args[1];
-		String sqlPort = args[2];
-		//SELECT
-		 Connection c = null;
+    static String SELECT_ALL_IMAGES_SQL = "SELECT * FROM nasa_images ORDER BY priority, image_name";
 
-		connectionPool = new BasicDataSource();
-		connectionPool.setUsername(properties.getProperty("datastore_username"));
-		connectionPool.setPassword(properties.getProperty("datastore_password"));
-		connectionPool.setDriverClassName(properties.getProperty("datastore_driver"));
-		connectionPool.setUrl(properties.getProperty("datastore_url_prefix")
-				+ sqlIP + ":" + sqlPort);
-		connectionPool.setInitialSize(1);
-		
-		c = getConnection();
-	       try {
-	     		PreparedStatement selectStatement = null;
-	     		selectStatement = c.prepareStatement(SELECT_ALL_IMAGES_SQL);
-	     		
-	     	ResultSet rs = selectStatement.executeQuery();
-	         
-			while (rs.next()) {
-				System.out.println(new ImageData(rs.getString("image_name"), rs
-						.getString("download_link"), ImageState
-						.getStateFromStr(rs.getString("state")), rs
-						.getString("federation_member"), rs.getInt("priority"),
-						rs.getString("station_id"), rs
-								.getString("sebal_version"), rs
-								.getString("sebal_engine_version"), rs
-								.getString("blowout_version"), rs
-								.getDate("ctime"), rs.getDate("utime"), rs
-								.getString("error_msg")));
-			}
-	         
-	       } catch ( Exception e ) {
-	         System.err.println( e.getClass().getName()+": "+ e.getMessage() );
-	         e.printStackTrace();
-	         System.exit(0);
-	       }
-	       System.out.println("Operation done successfully");
-	}
-	
-	private static final String UPDATE_STATE_SQL = "UPDATE nasa_images SET state = ? WHERE image_name = ?";
-	
-	public static Connection getConnection() throws SQLException {
-		try {
-			return connectionPool.getConnection();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
+    /**
+     * @param args args[0] path to properties file
+     *             args[1] database IP
+     *             args[2] database port
+     * @throws IOException
+     * @throws SQLException
+     */
+    public static void main(String[] args) throws IOException {
+
+        //FIXME: check parameters to fail gracefully
+        final Properties properties = new Properties();
+        FileInputStream input = new FileInputStream(args[0]);
+        properties.load(input);
+
+        String sqlIP = args[1];
+        String sqlPort = args[2];
+
+        BasicDataSource connectionPool = new BasicDataSource();
+
+        connectionPool.setUsername(properties.getProperty(DATASTORE_USERNAME));
+        connectionPool.setPassword(properties.getProperty(DATASTORE_PASSWORD));
+        connectionPool.setDriverClassName(properties.getProperty(DATASTORE_DRIVER));
+        connectionPool.setUrl(properties.getProperty(DATASTORE_URL_PREFIX) + sqlIP + ":" + sqlPort);
+        connectionPool.setInitialSize(1);
+
+
+        try {
+            Connection c = connectionPool.getConnection();
+            PreparedStatement selectStatement = null;
+            selectStatement = c.prepareStatement(SELECT_ALL_IMAGES_SQL);
+
+            ResultSet rs = selectStatement.executeQuery();
+
+            while (rs.next()) {
+                ImageData imageData = new ImageData(
+                        rs.getString("image_name"),
+                        rs.getString("download_link"),
+                        ImageState.getStateFromStr(rs.getString("state")),
+                        rs.getString("federation_member"),
+                        rs.getInt("priority"),
+                        rs.getString("station_id"),
+                        rs.getString("sebal_version"),
+                        rs.getString("sebal_engine_version"),
+                        rs.getString("blowout_version"),
+                        rs.getDate("ctime"),
+                        rs.getDate("utime"),
+                        rs.getString("error_msg"));
+                System.out.println(imageData);
+            }
+
+        } catch (Throwable e) {
+            LOGGER.error("Error while handling catalog database", e);
+            System.exit(1);
+        }
+    }
+
 }
