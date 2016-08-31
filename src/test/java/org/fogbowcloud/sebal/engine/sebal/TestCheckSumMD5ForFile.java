11e13e3907f860c702ebdcc8e82756ddf70d31c5
EsdrasVP <esdras015@gmail.com>
2016-08-31 16:30:53 -0300

    Implementing test for big files checksum

--- src/test/java/org/fogbowcloud/sebal/engine/sebal/TestCheckSumMD5ForFile.java
+++ src/test/java/org/fogbowcloud/sebal/engine/sebal/TestCheckSumMD5ForFile.java
@@ -4,12 +4,17 @@ import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -33,57 +38,55 @@ public class TestCheckSumMD5ForFile {
 	
 	@Test
 	public void testIsFileCorrupted() throws IOException {
-		exception.expect(Exception.class);
+		// setup
+		File tempFile1 = folder.newFile("file1.nc");
+		File tempFile2 = folder.newFile("file2.nc");
 		
-		File localFileMock = mock(File.class);
-		String localChecksum = "fake-local-checksum";
+		FileInputStream file1InputStream = new FileInputStream(tempFile1);
+		FileInputStream file2InputStream = new FileInputStream(tempFile2);
 		
-		DigestUtils digestUtilsMock = mock(DigestUtils.class);
+		String checkSum1 = DigestUtils.md5Hex(IOUtils
+				.toByteArray(file1InputStream));
+		String checkSum2 = DigestUtils.md5Hex(IOUtils
+				.toByteArray(file2InputStream));
 		
-		doReturn(true).when(localFileMock.getName()).endsWith(eq(".md5"));
+		file1InputStream.close();
+		file2InputStream.close();
 		
-		doReturn(localChecksum).when(digestUtilsMock).md5Hex(IOUtils
-				.toByteArray(fileInputStreamMock));
+		folder.newFile(tempFile1.getName() + "." + checkSum1 + ".md5");
+		folder.newFile(tempFile2.getName() + "." + checkSum2 + ".md5");
 		
-		File remoteFileMock = mock(File.class);
-		String remoteChecksum = null;
-		String[] pieces = {"fake-name", "fake-local-checksum", ".md5"};
+		// exercise
+		boolean isFileCorrupted = CheckSumMD5ForFile.isFileCorrupted(folder.getRoot());
 		
-		doReturn(true).when(remoteFileMock).getName().startsWith(eq(remoteFileMock.getName()));
-		doReturn(true).when(remoteFileMock).getName().endsWith(eq(".md5"));
-		doReturn(pieces).when(remoteFileMock).getName().split(".");
-		remoteChecksum = pieces[1];
-		
-		doReturn(true).when(localChecksum).equals(eq(remoteChecksum));
+		// expect
+		Assert.assertEquals(false, isFileCorrupted);
 	}
 	
-//	@Test
-//	public void testOutOfMemoryError() throws IOException {
-//		// setup				
-//		File tempFile1 = folder.newFile("file1.nc");
-//		File tempFile2 = folder.newFile("file2.nc");
-//		
-//		FileInputStream file1InputStream = new FileInputStream(tempFile1);
-//		FileInputStream file2InputStream = new FileInputStream(tempFile2);
-//		
-//		String checkSum1 = DigestUtils.md5Hex(IOUtils
-//				.toByteArray(file1InputStream));
-//		String checkSum2 = DigestUtils.md5Hex(IOUtils
-//				.toByteArray(file2InputStream));
-//		
-//		file1InputStream.close();
-//		file2InputStream.close();
-//		
-//		folder.newFile(tempFile1.getName() + checkSum1 + ".md5");
-//		folder.newFile(tempFile2.getName() + checkSum2 + ".md5");
-//		
-//		// exercise
-//		boolean isFile1Corrupted = CheckSumMD5ForFile.isFileCorrupted(tempFile1);
-//		boolean isFile2Corrupted = CheckSumMD5ForFile.isFileCorrupted(tempFile2);
-//		
-//		// expect
-//		Assert.assertEquals(false, isFile1Corrupted);
-//		Assert.assertEquals(false, isFile2Corrupted);
-//	}
+	@Test
+	public void testChecksumForBigFiles() throws IOException {
+		// setup
+		byte []arr = new byte [379584512];
+		File tempFile1 = folder.newFile("file1.nc");
+		
+		BufferedOutputStream output = new BufferedOutputStream(new FileOutputStream(tempFile1));
+        output.write(arr);		
+        output.close();
+        
+		FileInputStream file1InputStream = new FileInputStream(tempFile1);		
+		
+		String checkSum1 = DigestUtils.md5Hex(IOUtils
+				.toByteArray(file1InputStream));
+		
+		file1InputStream.close();
+		
+		folder.newFile(tempFile1.getName() + "." + checkSum1 + ".md5");
+		
+		// exercise
+		boolean isFileCorrupted = CheckSumMD5ForFile.isFileCorrupted(folder.getRoot());
+		
+		// expect
+		Assert.assertEquals(false, isFileCorrupted);
+	}
 
 }
