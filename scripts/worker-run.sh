5b4b226589a215a74f8dd08565d16040200f8521
EsdrasVP <esdras015@gmail.com>
2016-09-23 18:06:06 -0300

    Commiting changes in worker-run.sh to only prepare data for users code to process

--- scripts/worker-run.sh
+++ scripts/worker-run.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
 # Global variables
-CONF_FILE=sebal.conf
-BOUNDING_BOX_PATH=example/boundingbox_vertices
 IMAGES_DIR_NAME=images
 RESULTS_DIR_NAME=results
-OUTPUT_IMAGE_DIR=${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}
-LIBRARY_PATH=/usr/local/lib/${ADDITIONAL_LIBRARY_PATH}
-
-# User's responsability
-R_EXEC_DIR=${SANDBOX}/${R_REPOSITORY_PATH}
-
-SEBAL_DIR_PATH=
-LOG4J_PATH=
+BIN_INIT_SCRIPT="bin/run.sh"
 
 # This function untare image and creates an output dir into mounted dir
 function untarImageAndPrepareDirs {
@@ -26,39 +17,16 @@ function untarImageAndPrepareDirs {
   sudo tar -xvzf ${IMAGE_NAME}".tar.gz"
 
   echo "Creating image output directory"
-  sudo mkdir -p $OUTPUT_IMAGE_DIR
+  sudo mkdir -p ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}
 }
 
-# This function calls a pre process java code to prepare a station file of a given image
-function preProcessImage {
-  cd ${SANDBOX}/SEBAL/
-  SEBAL_DIR_PATH=$(pwd)
-  LOG4J_PATH=$SEBAL_DIR_PATH/log4j.properties
+function executeRunScript {
+  #when https://github.com/xpto/foo-baa.git we have foo-baa which is the root dir of the repo
+  repositoryName=`echo ${SEBAL_URL} | rev | cut -d "/" -f1 | cut -d"." -f2 | rev`
 
-  #echo "Generating app snapshot"
-  #mvn -e install -Dmaven.test.skip=true
+  cd ${SANDBOX}
 
-  sudo java -Dlog4j.configuration=file:$LOG4J_PATH -Djava.library.path=$LIBRARY_PATH -cp target/SEBAL-0.0.1-SNAPSHOT.jar:target/lib/* org.fogbowcloud.sebal.PreProcessMain ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/ ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTL.txt" ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/ 0 0 9000 9000 1 1 $SEBAL_DIR_PATH/$BOUNDING_BOX_PATH $SEBAL_DIR_PATH/$CONF_FILE ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTLFmask"
-  sudo chmod 777 ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_station.csv"
-  echo -e "\n" >> ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_station.csv"
-}
-
-# This function prepare a dados.csv file and calls R script to begin image execution
-function executeRScript {
-  echo "Creating dados.csv for image ${IMAGE_NAME}"
-
-  cd $R_EXEC_DIR
-
-  echo "File images;MTL;File Station Weather;File Fmask;Path Output" > dados.csv
-  echo "${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME};${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTL.txt";${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_station.csv";${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTLFmask";$OUTPUT_IMAGE_DIR" >> dados.csv
-  echo "Executing R script..."
-  sudo Rscript $R_EXEC_DIR/${R_ALGORITHM_VERSION} $R_EXEC_DIR
-  #R CMD BATCH "--args WD='$R_EXEC_DIR'" $R_EXEC_DIR/${R_ALGORITHM_VERSION}
-  echo "Process finished!"
-
-  echo "Renaming dados file"
-  mv dados.csv dados"-${IMAGE_NAME}".csv
-  sudo mv dados"-${IMAGE_NAME}".csv $OUTPUT_IMAGE_DIR
+  bash -x $repositoryName/$BIN_RUN_SCRIPT ${IMAGE_NAME} ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/ ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/ ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME} ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTL.txt" ${SEBAL_MOUNT_POINT}/$IMAGES_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_MTLFmask" ${SEBAL_MOUNT_POINT}/$RESULTS_DIR_NAME/${IMAGE_NAME}/${IMAGE_NAME}"_station.csv"
 }
 
 # This function do a checksum of all output files in image dir
@@ -80,7 +48,6 @@ function finally {
 }
 
 untarImageAndPrepareDirs
-preProcessImage
-executeRScript
+executeRunScript
 checkSum
 finally
