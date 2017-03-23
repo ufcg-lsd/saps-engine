adb453c1683687749eef900194063cd8f71e7063
EsdrasVP <esdras015@gmail.com>
2017-03-23 11:18:52 -0300

    Adapting download link request to the new USGS API implementation

--- src/main/java/org/fogbowcloud/sebal/engine/sebal/NASARepository.java
+++ src/main/java/org/fogbowcloud/sebal/engine/sebal/NASARepository.java
@@ -1,8 +1,6 @@
 package org.fogbowcloud.sebal.engine.sebal;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Created by manel on 18/08/16.
@@ -15,15 +13,5 @@ public interface NASARepository {
      * @throws IOException
      */
     void downloadImage(final ImageData imageData) throws IOException;
-
-    /**
-     * It gets a imageName:downloadURL for a batch of image names. It gets
-     * only downloadURLs for those images that are present in the remote repository.
-     *
-     * @param imageListFile
-     * @return
-     * @throws IOException
-     */
-    Map<String, String> getDownloadLinks(File imageListFile) throws IOException;
 
 }
--- src/main/java/org/fogbowcloud/sebal/engine/sebal/USGSNasaRepository.java
+++ src/main/java/org/fogbowcloud/sebal/engine/sebal/USGSNasaRepository.java
@@ -5,14 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.collections.map.HashedMap;
-import org.apache.commons.io.Charsets;
-import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.Validate;
 import org.apache.http.HttpResponse;
@@ -26,6 +24,8 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by manel on 18/08/16.
@@ -33,13 +33,15 @@ import org.apache.log4j.Logger;
 public class USGSNasaRepository implements NASARepository {
 
     private static final Logger LOGGER = Logger.getLogger(USGSNasaRepository.class);
-
+    
     private final String sebalExportPath;
 
     private final String usgsLoginUrl;
+    private final String usgsJsonUrl;
     private final String usgsUserName;
     private final String usgsPassword;
-    private final String usgsCLIPath;
+    private final String usgsAPIKeyPeriod;
+    private String usgsAPIKey;
     
     private static final String LANDSAT_5_PREFIX = "LT5";
     private static final String LANDSAT_7_PREFIX = "LE7";
@@ -58,39 +60,85 @@ public class USGSNasaRepository implements NASARepository {
     // conf constants
     private static final String SEBAL_EXPORT_PATH = "sebal_export_path";
     private static final String USGS_LOGIN_URL = "nasa_login_url";
+    private static final String USGS_JSON_URL = "nasa_json_url";
     private static final String USGS_USERNAME = "usgs_username";
     private static final String USGS_PASSWORD = "usgs_password";
-    private static final String USGS_CLI_PATH = "usgs_cli_path";
+    private static final String USGS_API_KEY_PERIOD = "usgs_api_key_period";
+
     
     public USGSNasaRepository(Properties properties) {
 		this(properties.getProperty(SEBAL_EXPORT_PATH), properties
 				.getProperty(USGS_LOGIN_URL), properties
+				.getProperty(USGS_JSON_URL), properties
 				.getProperty(USGS_USERNAME), properties
 				.getProperty(USGS_PASSWORD), properties
-				.getProperty(USGS_CLI_PATH));
+				.getProperty(USGS_API_KEY_PERIOD));
     }
 
-    protected USGSNasaRepository(String sebalExportPath, String usgsLoginUrl, String usgsUserName,
-                              String usgsPassword, String usgsCLIPath) {
+	protected USGSNasaRepository(String sebalExportPath, String usgsLoginUrl,
+			String usgsJsonUrl, String usgsUserName, String usgsPassword,
+			String usgsAPIKeyPeriod) {
 
         Validate.notNull(sebalExportPath, "sebalExportPath cannot be null");
 
         Validate.notNull(usgsLoginUrl, "usgsLoginUrl cannot be null");
+        Validate.notNull(usgsJsonUrl, "usgsJsonUrl cannot be null");
         Validate.notNull(usgsUserName, "usgsUserName cannot be null");
         Validate.notNull(usgsPassword, "usgsPassword cannot be null");
-        Validate.notNull(usgsCLIPath, "usgsCLIPath cannot be null");
+        Validate.notNull(usgsAPIKeyPeriod, "usgsAPIKeyPeriod cannot be null");
 
         this.sebalExportPath = sebalExportPath;
         this.usgsLoginUrl = usgsLoginUrl;
+        this.usgsJsonUrl = usgsJsonUrl;
         this.usgsUserName = usgsUserName;
         this.usgsPassword = usgsPassword;
-        this.usgsCLIPath = usgsCLIPath;
+        this.usgsAPIKeyPeriod = usgsAPIKeyPeriod;
 
         Validate.isTrue(directoryExists(sebalExportPath),
                 "Sebal sebalExportPath directory " + sebalExportPath + "does not exist.");
+        
+		handleAPIKeyUpdate(Executors.newScheduledThreadPool(1));
     }
 
-    private boolean directoryExists(String path) {
+	private void handleAPIKeyUpdate(
+			ScheduledExecutorService handleAPIKeyUpdateExecutor) {
+		LOGGER.debug("Turning on handle USGS API key update.");
+
+		handleAPIKeyUpdateExecutor.scheduleWithFixedDelay(new Runnable() {
+			@Override
+			public void run() {
+				setUSGSAPIKey(generateAPIKey());
+			}
+
+		}, 0, Integer.parseInt(usgsAPIKeyPeriod), TimeUnit.MILLISECONDS);
+	}
+
+	private String generateAPIKey() {
+
+		try {
+			BasicCookieStore cookieStore = new BasicCookieStore();
+			HttpClient httpClient = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();
+			
+			String loginJsonRequest = "{\"username\":\"" + usgsUserName
+					+ "\",\"password\":\"" + usgsPassword
+					+ "\",\"authType\":\"EROS\"}";
+			
+			HttpGet homeGet = new HttpGet(usgsJsonUrl + File.separator
+					+ "login?jsonRequest=" + loginJsonRequest);
+	        httpClient.execute(homeGet);
+	        
+	        HttpResponse response = httpClient.execute(homeGet);
+			JSONObject apiKeyRequestResponse = new JSONObject(response);
+
+			return apiKeyRequestResponse.getString("data");
+		} catch (Throwable e) {
+			LOGGER.error("Error while generating USGS API key", e);
+		}
+
+		return null;
+	}
+    
+	private boolean directoryExists(String path) {
         File f = new File(path);
         return (f.exists() && f.isDirectory());
     }
@@ -150,8 +198,6 @@ public class USGSNasaRepository implements NASARepository {
 
     private HttpClient initClient() throws IOException {
 
-        //FIXME: refactor no use usgs API
-
         BasicCookieStore cookieStore = new BasicCookieStore();
         HttpClient httpClient = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();
 
@@ -171,96 +217,35 @@ public class USGSNasaRepository implements NASARepository {
         EntityUtils.toString(homePostResponse.getEntity());
         return httpClient;
     }
-
-    @Override
-    public Map<String, String> getDownloadLinks(File imageListFile) throws IOException {
-        //FIXME: this will be removed from API after we get USGS working
-    	//FIXME: see if Charsets import is correct
-    	Collection<String> imageNames = FileUtils.readLines(imageListFile, Charsets.UTF_8);
-        return doGetDownloadLinks(imageNames);
-    }
-
-	private Map<String, String> doGetDownloadLinks(Collection<String> imageNames) {
-
-		Map<String, String> links = new HashedMap();
-		
-		if (doLoginIntoUSGS() == 0) {
-			for (String imageName : imageNames) {
-				String link = doGetDownloadLink(imageName);
-				if (link != null && !link.isEmpty()) {
-					links.put(imageName, link);
-				}
-			}
-		} else {			
-			LOGGER.error("Error while logging in USGS Repository");
-		}
-
-		return links;
-	}
 	
 	public String getImageDownloadLink(String imageName) {
 
-		if (doLoginIntoUSGS() == 0) {
+		if (usgsAPIKey != null && !usgsAPIKey.isEmpty()) {
 			String link = doGetDownloadLink(imageName);
 			if (link != null && !link.isEmpty()) {
 				return link;
 			}
 		} else {
-			LOGGER.error("Error while logging in USGS Repository");
+			LOGGER.error("USGS API key invalid");
 		}
 
 		return null;
 	}
 
-    private int doLoginIntoUSGS() {
-
-    	Response loginResponse = null;
-    	
-        try {
-        	loginResponse = loginUSGSRepository(usgsUserName, usgsPassword);
-        } catch(Throwable e) {
-        	LOGGER.error("Error while running login command", e);
-        	LOGGER.debug("Error response was " + loginResponse.err);
-        }
-        
-        return loginResponse.exitValue;
-	}
-
-	private Response loginUSGSRepository(String usgsUserName, String usgsPassword) throws IOException, InterruptedException {
-    	ProcessBuilder builder = new ProcessBuilder(this.usgsCLIPath, "login", usgsUserName, usgsPassword);        
-        LOGGER.debug("Executing command " + builder.command());
-        
-        Process p = builder.start();
-        p.waitFor();
-        
-        String loginOutput = ProcessUtil.getOutput(p);
-        String loginErr = ProcessUtil.getError(p);
-        
-        int exitValue = p.exitValue();
-
-        Response loginResponse = new Response(loginOutput, loginErr, exitValue);
-        return loginResponse;
-	}
-
 	private String doGetDownloadLink(String imageName) {
 
-        String link = null;
+		String link = null;
 
-        try {
-            Response response = usgsDownloadURL(getDataSet(imageName), imageName, EARTH_EXPLORER_NODE, LEVEL_1_PRODUCT);
+		try {
+			link = usgsDownloadURL(getDataSet(imageName), imageName,
+					EARTH_EXPLORER_NODE, LEVEL_1_PRODUCT);
+		} catch (Throwable e) {
+			LOGGER.error("Error while getting download link for image "
+					+ imageName, e);
+		}
 
-            if (response.exitValue != 0) {
-                LOGGER.error("Error while running command. " + response);
-            } else {
-                LOGGER.debug("Command successfully executed. " + response);
-                link = response.out;
-            }
-        } catch (Throwable e) {
-            LOGGER.error("Error while running command", e);
-        }
-
-        return link;
-    }
+		return link;
+	}
 
     private String getDataSet(String imageName) {
     	if(imageName.startsWith(LANDSAT_5_PREFIX)) {
@@ -274,57 +259,39 @@ public class USGSNasaRepository implements NASARepository {
         return null;
     }
 
-    protected Response usgsDownloadURL(String dataset, String sceneId, String node, String product)
-            throws IOException, InterruptedException {
+	protected String usgsDownloadURL(String dataset, String sceneId,
+			String node, String product) throws IOException,
+			InterruptedException, JSONException {
 
-        //usgs download-url [dataset] [entity/scene id] --node [node] --product [product]
-        //FIXME: is it possible to download a list of scenes at once?
+		BasicCookieStore cookieStore = new BasicCookieStore();
+		HttpClient httpClient = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();
+		String downloadJsonRequest = "{\"datasetName\":\"" + dataset
+				+ "\",\"apiKey\":\"" + usgsAPIKey + "\",\"node\":\"" + node
+				+ "\",\"entityIds\":[\"" + sceneId + "\"],\"products\":[\""
+				+ product + "\"]}";
+
+		// GET DOWNLOAD LINKS
+		HttpGet homeGet = new HttpGet(usgsJsonUrl + File.separator
+				+ "download?jsonRequest=" + downloadJsonRequest);
+        httpClient.execute(homeGet);
         
-        // GET DOWNLOAD LINKS
-        ProcessBuilder builder = new ProcessBuilder(this.usgsCLIPath, "download-url", dataset, sceneId, "--node", node, "--product", product);        
-        LOGGER.debug("Executing command " + builder.command());
+        HttpResponse response = httpClient.execute(homeGet);
+		JSONObject downloadRequestResponse = new JSONObject(response);
 
-        Process p = builder.start();
-        p.waitFor();
+		int firstDownloadUrl = 0;
+		String formatedResponse = downloadRequestResponse.getJSONArray("data")
+				.getString(firstDownloadUrl).replace("\\/", "/");
+		String downloadLink = downloadRequestResponse.getJSONArray("data").getString(firstDownloadUrl);
 
-        String getDownloadUrlOutput = ProcessUtil.getOutput(p);
-        String getDownloadUrlErr = ProcessUtil.getError(p);
-
-        int exitValue = p.exitValue();
-
-        Response getDownloadUrlResponse = new Response(getDownloadUrlOutput, getDownloadUrlErr, exitValue);
-        return getDownloadUrlResponse;
-    }
-
-    private class Response {
-
-        public String out;
-        public String err;
-        public int exitValue;
-
-        public Response(String out, String err, int exitValue) {
-
-            if (out == null) {
-                throw new IllegalArgumentException("out parameter cannot be null");
-            }
-
-            if (err == null) {
-                throw new IllegalArgumentException("err parameter cannot be null");
-            }
-
-            this.out = out;
-            this.err = err;
-            this.exitValue = exitValue;
-        }
-
-        @Override
-        public String toString() {
-            return "Response{" +
-                    "out='" + out + '\'' +
-                    ", err='" + err + '\'' +
-                    ", exitValue=" + exitValue +
-                    '}';
-        }
-    }
-
+		if(formatedResponse != null && !formatedResponse.isEmpty()) {
+			LOGGER.debug("Image " + sceneId + "download link" + downloadLink + " obtained");
+			return downloadLink;
+		}
+		
+		return null;
+	}
+    
+	private void setUSGSAPIKey(String usgsAPIKey) {
+		this.usgsAPIKey = usgsAPIKey;
+	}
 }
