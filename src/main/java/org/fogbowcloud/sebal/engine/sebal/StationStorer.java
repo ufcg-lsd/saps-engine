dc1cdcfa52774199070962c09072a2e9ad4d854c
EsdrasVP <esdras015@gmail.com>
2017-04-05 10:29:31 -0300

    Fixing implementation problems in StationStorer

--- src/main/java/org/fogbowcloud/sebal/engine/sebal/StationStorer.java
+++ src/main/java/org/fogbowcloud/sebal/engine/sebal/StationStorer.java
@@ -1,5 +1,6 @@
 package org.fogbowcloud.sebal.engine.sebal;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -25,8 +26,8 @@ import org.apache.log4j.Logger;
 import org.fogbowcloud.sebal.engine.scheduler.util.SebalPropertiesConstants;
 import org.fogbowcloud.sebal.engine.swift.SwiftAPIClient;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
@@ -62,76 +63,70 @@ public class StationStorer {
 	}
 
 	private List<String> getStationsFromJSON() {
-		List<String> stations = new ArrayList<String>();		
-		JSONParser jsonParser = new JSONParser();
-		
-		try {
-			getStations(stations, jsonParser);
-		} catch(Exception e) {
-			LOGGER.error("Error while getting stations from JSON file", e);
-		}
-		
-		return stations;
+		List<String> stations = new ArrayList<String>();						
+		return getStations(stations);
 	}
 
-	private void getStations(List<String> stations, JSONParser jsonParser)
-			throws IOException, ParseException, FileNotFoundException,
-			JSONException {
-		
-		String stationsFilePath = properties.getProperty(SebalPropertiesConstants.STATIONS_FILE_PATH);
-		
-		Object object = jsonParser.parse(new FileReader(stationsFilePath));			
-		JSONObject jsonObject = (JSONObject) object;
-		
-		JSONArray jsonArray = (JSONArray) jsonObject.get("stations");
-		
-		//Reading the String
-		Iterator<?> iterator = jsonArray.iterator();
-		while (iterator.hasNext()) {		        
-			String stationId = (String) jsonObject.get("id");
-			stations.add(stationId);
+	private List<String> getStations(List<String> stations) {
+		try {
+			String stationsFilePath = properties
+					.getProperty(SebalPropertiesConstants.STATIONS_FILE_PATH);
+
+			File stationsFile = new File(stationsFilePath);
+
+			BufferedReader br = new BufferedReader(new FileReader(stationsFile));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				stations.add(line);
+			}
+
+			br.close();
+		} catch (IOException e) {
+			LOGGER.error("Error while reading stations file", e);
 		}
+
+		return stations;
 	}
 
 	private void downloadUploadStationFiles(List<String> stations) throws Exception {
 		for(int year = FIRST_RECORD_YEAR; year <= LAST_RECORD_YEAR; year++) {
 			
-	        String yearDirPath = getYearDirPath(properties.getProperty(SebalPropertiesConstants.BASE_YEAR_DIR_PATH), year);
-	        boolean wasCreated = createDirectoryForYear(yearDirPath);
+	        String yearDirPath = getYearDirPath(properties.getProperty(SebalPropertiesConstants.BASE_YEAR_DIR_PATH), year);	        
+			File yearDir = new File(yearDirPath);
+	        yearDir.mkdirs();
 	        
-	        if (wasCreated) {
-				for (int j = 0; j <= stations.size(); j++) {
-					downloadStationFile(stations, year, yearDirPath, j);
-					uploadStationFile(stations, year, yearDirPath, j);
-				}
+	        if(yearDir.exists()) {
+	        	for (int j = 0; j <= stations.size(); j++) {
+	        		if(downloadStationFile(stations, year, yearDirPath, j)) {	        			
+	        			uploadStationFile(stations, year, yearDirPath, j);
+	        		}
+	        	}
+	        	
+	        	deleteDirectoryForYear(year, yearDirPath);
 	        }
-	        
-	        deleteDirectoryForYear(year, yearDirPath);
 		}
 	}
-
-	private boolean createDirectoryForYear(String yearDirPath) {
-		File yearDir = new File(yearDirPath);
-        return yearDir.mkdirs();
-	}
 	
-	private void downloadStationFile(List<String> stations, int year,
+	private boolean downloadStationFile(List<String> stations, int year,
 			String yearDirPath, int j) throws IOException {
-		String localStationFilePath = getStationCompressedFilePath(stations.get(j),
-				year, yearDirPath);
-
-		// clean if already exists (garbage collection)
-		File localStationFile = new File(localStationFilePath);
-		if (localStationFile.exists()) {
-			LOGGER.info("File "
-					+ localStationFilePath
-					+ " already exists. Will be removed before repeating download");
-			localStationFile.delete();
+		LOGGER.info("Downloading station file " + stations.get(j) + " into " + yearDirPath);
+		
+		ProcessBuilder builder = new ProcessBuilder("wget", "-P", yearDirPath,
+				this.noaaFTPUrl + File.separator + year + File.separator
+						+ stations.get(j) + "0-99999-" + year + ".gz");
+		
+		try {
+			Process p = builder.start();
+			p.waitFor();					
+		} catch (IOException e) {
+			LOGGER.error("Error while writing file for station", e);
+		} catch (InterruptedException e) {
+			LOGGER.error("Error while downloading file for station", e);
 		}
 		
-		downloadInto(year, stations.get(j), localStationFile);
+		return downloadStationSucceeded(getStationCompressedFilePath(stations.get(j), year, yearDirPath));
 	}
-	
+
 	private String getStationCompressedFilePath(String stationId, int year, String yearDirPath) {
 		return yearDirPath + File.separator + stationId + "0-99999-" + year + ".gz";
 	}
@@ -139,28 +134,20 @@ public class StationStorer {
 	private String getYearDirPath(String baseYearDirPath, int year) {
 		return baseYearDirPath + File.separator + year;
 	}
-
-	private void downloadInto(int recordYear, String stationId, File stationFile) throws IOException {
-
-		LOGGER.info("Downloading station file " + stationId + " into file "
-				+ stationFile.getAbsolutePath());
+	
+	private boolean downloadStationSucceeded(String stationCompressedFilePath) {
+		File stationCompressedFile = new File(stationCompressedFilePath);		
+		if(stationCompressedFile.exists()) {
+			LOGGER.debug("File " + stationCompressedFilePath + " downloaded successfully");
+			return true;
+		}
 		
-    	BasicCookieStore cookieStore = new BasicCookieStore();
-        HttpClient httpClient = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();    
-
-		HttpGet homeGet = new HttpGet(this.noaaFTPUrl + File.separator
-				+ recordYear + File.separator + stationId + "0-99999-"
-				+ recordYear + ".gz");
-        HttpResponse response = httpClient.execute(homeGet);
-
-        OutputStream outStream = new FileOutputStream(stationFile);
-		IOUtils.copy(response.getEntity().getContent(), outStream);
-		outStream.close();    
-    }
+		LOGGER.debug("File " + stationCompressedFilePath + " was not downloaded");
+		return false;
+	}
 	
 	private void uploadStationFile(List<String> stations, int year,
 			String yearDirPath, int j) throws Exception {
-		
 		// Getting compressed station file
 		String localCompressedStationFilePath = getStationCompressedFilePath(stations.get(j), year,
 				yearDirPath);		
@@ -176,7 +163,6 @@ public class StationStorer {
 	}
 	
 	public static File unGzip(File file, boolean deleteGzipfileOnSuccess) throws IOException {
-		
 	    GZIPInputStream gin = new GZIPInputStream(new FileInputStream(file));
 	    FileOutputStream fos = null;
 	    try {
@@ -214,8 +200,7 @@ public class StationStorer {
 		swiftAPIClient.uploadFile(containerName, localStationFile, pseudoFolder);
 	}
 
-	private void deleteDirectoryForYear(int year, String yearDirPath) throws IOException {
-		
+	private void deleteDirectoryForYear(int year, String yearDirPath) throws IOException {	
 		File yearDir = new File(yearDirPath);		
 		LOGGER.info("Removing year " + year + " data under path " + yearDirPath);
 
@@ -230,5 +215,21 @@ public class StationStorer {
 			return false;
 		}
 		return true;
+	}
+	
+	public static void main(String[] args) {
+		Properties properties = new Properties();
+		FileInputStream input;
+		try {
+			input = new FileInputStream("config/sebal.conf");
+			properties.load(input);
+		} catch (FileNotFoundException e) {
+			LOGGER.error("Error while reading conf file", e);
+		} catch (IOException e) {
+			LOGGER.error("Error while loading properties", e);
+		}
+		
+		StationStorer stationStorer = new StationStorer(properties);
+		stationStorer.init();
 	}
 }
