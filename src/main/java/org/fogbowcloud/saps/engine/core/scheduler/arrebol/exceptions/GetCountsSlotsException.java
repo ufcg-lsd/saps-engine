9f72b0aebd4c81bddedcd15647e8d562554ada2e
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-11-01 13:54:29 -0300

    Adding new Arrebol exception

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/scheduler/arrebol/exceptions/GetCountsSlotsException.java
@@ -0,0 +1,7 @@
+package org.fogbowcloud.saps.engine.core.scheduler.arrebol.exceptions;
+
+public class GetCountsSlotsException extends Throwable {
+    public GetCountsSlotsException(String s, Exception e) {
+        super(s, e);
+    }
+}
