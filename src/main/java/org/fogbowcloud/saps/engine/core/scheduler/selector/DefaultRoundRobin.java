c66b96df07b5ca423ba6d8e005dfb038b0f26c8f
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-11-01 13:54:47 -0300

    Fix bug

--- src/main/java/org/fogbowcloud/saps/engine/core/scheduler/selector/DefaultRoundRobin.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/scheduler/selector/DefaultRoundRobin.java
@@ -11,17 +11,20 @@ public class DefaultRoundRobin implements Selector {
 	@Override
 	public List<ImageTask> select(int count, Map<String, List<ImageTask>> tasks) {
 		List<ImageTask> selectedTasks = new LinkedList<ImageTask>();
+		List<String> usersForDelete = new LinkedList<String>();
 
 		while (count > 0 && tasks.size() > 0) {
 			for (String user : tasks.keySet()) {
 				if (count > 0) {
 					selectedTasks.add(tasks.get(user).remove(0));
 					count--;
-
-					if (tasks.get(user).size() == 0)
-						tasks.remove(user);
 				}
+				if (tasks.get(user).size() == 0)
+					usersForDelete.add(user);
 			}
+
+			for (String user : usersForDelete)
+				tasks.remove(user);
 		}
 
 		return selectedTasks;
