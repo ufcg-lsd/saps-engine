dccca31f3ab2e3ed7112a196d09ca152ceaba089
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-11-21 15:07:51 -0300

    Update response task dto

--- src/main/java/org/fogbowcloud/saps/engine/core/dto/TaskSpecResponseDTO.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/dto/TaskSpecResponseDTO.java
@@ -2,51 +2,63 @@ package org.fogbowcloud.saps.engine.core.dto;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 public class TaskSpecResponseDTO implements Serializable {
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
-    private String id;
-    private List<CommandResponseDTO> commands;
-    private String image;
+	private String id;
+	private List<CommandResponseDTO> commands;
+	private Map<String, String> metadata;
+	private Map<String, String> requirements;
 
-    public TaskSpecResponseDTO(String id, List<CommandResponseDTO> commands, String image) {
-        this.id = id;
-        this.commands = commands;
-        this.image = image;
-    }
+	public TaskSpecResponseDTO(String id, List<CommandResponseDTO> commands, Map<String, String> metadata,
+			Map<String, String> requirements) {
+		this.id = id;
+		this.commands = commands;
+		this.metadata = metadata;
+		this.requirements = requirements;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public List<CommandResponseDTO> getCommands() {
-        return commands;
-    }
+	public List<CommandResponseDTO> getCommands() {
+		return commands;
+	}
 
-    public String getImage(){
-        return image;
-    }
+	public Map<String, String> getMetadata() {
+		return metadata;
+	}
 
-    public void setId(String id){
-        this.id = id;
-    }
+	public Map<String, String> getRequirements() {
+		return requirements;
+	}
 
-    public void setCommands(List<CommandResponseDTO> commands){
-        this.commands = commands;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setImage(String image){
-        this.image = image;
-    }
+	public void setCommands(List<CommandResponseDTO> commands) {
+		this.commands = commands;
+	}
 
-    @Override
-    public String toString() {
-        return "TaskSpecResponseDTO [id=" + id + ", commands="
-                + commands + ", image=" + image + "]";
-    }
+	public void setMetadata(Map<String, String> metadata) {
+		this.metadata = metadata;
+	}
+
+	public void setRequirements(Map<String, String> requirements) {
+		this.requirements = requirements;
+	}
+
+	@Override
+	public String toString() {
+		return "TaskSpecResponseDTO [id=" + id + ", requirements=" + requirements + ", commands=" + commands
+				+ ", metadata=" + metadata + "]";
+	}
 
 }
