c6394f915686177ce7f64b57a681f2cb60896753
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-11-21 15:36:51 -0300

    Adding toString method

--- src/main/java/org/fogbowcloud/saps/engine/core/scheduler/arrebol/JobSubmitted.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/scheduler/arrebol/JobSubmitted.java
@@ -4,34 +4,40 @@ import org.fogbowcloud.saps.engine.core.model.SapsImage;
 
 public class JobSubmitted {
 
-    private String jobId;
-    private SapsImage imageTask;
+	private String jobId;
+	private SapsImage imageTask;
 
-    public JobSubmitted(String jobId, SapsImage imageTask){
-        this.jobId = jobId;
-        this.imageTask = imageTask;
-    }
+	public JobSubmitted(String jobId, SapsImage imageTask) {
+		this.jobId = jobId;
+		this.imageTask = imageTask;
+	}
 
-    public String getJobId(){
-        return jobId;
-    }
+	public String getJobId() {
+		return jobId;
+	}
 
-    public SapsImage getImageTask(){
-        return imageTask;
-    }
+	public SapsImage getImageTask() {
+		return imageTask;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
 
-        JobSubmitted otherJob = (JobSubmitted) o;
+		JobSubmitted otherJob = (JobSubmitted) o;
 
-        if(this.getJobId() == otherJob.getJobId())
-            return true;
-        else
-            return false;
-    }
+		if (this.getJobId() == otherJob.getJobId())
+			return true;
+		else
+			return false;
+	}
+
+	@Override
+	public String toString() {
+		return "JobSubmitted [jobId=" + jobId + ", imageTask=" + imageTask + "]";
+	}
+
 }
