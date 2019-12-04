961883bfaef16d4c51afe284fe5fae0411ed76bf
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-12-04 17:27:24 -0300

    Rename retry class

---
+++ src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/AddTimestampRetry.java
@@ -0,0 +1,26 @@
+package org.fogbowcloud.saps.engine.utils.retry.catalog;
+
+import java.sql.SQLException;
+
+import org.fogbowcloud.saps.engine.core.database.ImageDataStore;
+import org.fogbowcloud.saps.engine.core.model.SapsImage;
+
+public class AddTimestampRetry implements CatalogRetry<Void>{
+
+	private ImageDataStore imageStore;
+	private SapsImage task;
+
+	public AddTimestampRetry(ImageDataStore imageStore, SapsImage task) {
+		this.imageStore = imageStore;
+		this.task = task;
+	}
+	
+	@Override
+	public Void run() throws SQLException {
+		task.setUpdateTime(imageStore.getTask(task.getTaskId()).getUpdateTime());
+		imageStore.addStateStamp(task.getTaskId(), task.getState(), task.getUpdateTime());
+		return null;
+	}
+	
+
+}
