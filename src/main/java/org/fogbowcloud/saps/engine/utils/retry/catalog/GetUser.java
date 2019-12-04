08583b79c9f5e0e040ba60fff883c154b76ee328
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2019-12-04 12:26:08 -0300

    Adding new retry classes

---
+++ src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/GetUser.java
@@ -0,0 +1,29 @@
+package org.fogbowcloud.saps.engine.utils.retry.catalog;
+
+import java.sql.SQLException;
+
+import org.fogbowcloud.saps.engine.core.database.ImageDataStore;
+import org.fogbowcloud.saps.engine.core.database.JDBCImageDataStore;
+import org.fogbowcloud.saps.engine.core.model.SapsUser;
+
+public class GetUser implements CatalogRetry<SapsUser> {
+
+	private ImageDataStore imageStore;
+	private String userEmail;
+
+	public GetUser(JDBCImageDataStore imageStore, String userEmail) {
+		this.imageStore = imageStore;
+		this.userEmail = userEmail;
+	}
+
+	@Override
+	public SapsUser run() throws SQLException {
+		try {
+			return imageStore.getUser(userEmail);
+		} catch (SQLException e) {
+			//nothing
+		}
+		return null;
+	}
+
+}
