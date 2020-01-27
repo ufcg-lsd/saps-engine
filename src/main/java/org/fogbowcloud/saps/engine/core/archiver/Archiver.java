1549b9b03cef14957c65dda0a8183ac9432368f8
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-01-27 16:01:44 -0300

    Using permanent storage interface and moved implementation for Swift type class

--- src/main/java/org/fogbowcloud/saps/engine/core/archiver/Archiver.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/Archiver.java
@@ -11,7 +11,9 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.fogbowcloud.saps.engine.core.archiver.swift.SwiftAPIClient;
+import org.fogbowcloud.saps.engine.core.archiver.storage.NfsPermanentStorage;
+import org.fogbowcloud.saps.engine.core.archiver.storage.PermanentStorage;
+import org.fogbowcloud.saps.engine.core.archiver.storage.SwiftPermanentStorage;
 import org.fogbowcloud.saps.engine.core.database.ImageDataStore;
 import org.fogbowcloud.saps.engine.core.database.JDBCImageDataStore;
 import org.fogbowcloud.saps.engine.core.model.SapsImage;
@@ -24,40 +26,51 @@ public class Archiver {
 
 	private final Properties properties;
 	private final ImageDataStore imageStore;
-	private final SwiftAPIClient swiftAPIClient;
+	private final PermanentStorage permanentStorage;
 	private ScheduledExecutorService sapsExecutor;
-	private final boolean executionMode;
-
-	private static int MAX_ARCHIVE_TRIES = 1;
-	private static int MAX_SWIFT_UPLOAD_TRIES = 2;
+	private final boolean executionDebugMode;
 
 	public static final Logger LOGGER = Logger.getLogger(Archiver.class);
 
 	public Archiver(Properties properties) throws SapsException, SQLException {
-		this(properties, new JDBCImageDataStore(properties), new SwiftAPIClient(properties), new ArchiverHelper());
+		this(properties, new JDBCImageDataStore(properties), new ArchiverHelper());
 
 		LOGGER.info("Creating Archiver");
 		LOGGER.info("Imagestore " + properties.getProperty(SapsPropertiesConstants.IMAGE_DATASTORE_IP) + ":"
 				+ properties.getProperty(SapsPropertiesConstants.IMAGE_DATASTORE_PORT));
 	}
 
-	protected Archiver(Properties properties, ImageDataStore imageStore, SwiftAPIClient swiftAPIClient,
-			ArchiverHelper archiverHelper) throws SapsException {
+	protected Archiver(Properties properties, ImageDataStore imageStore, ArchiverHelper archiverHelper)
+			throws SapsException {
 		if (!checkProperties(properties))
 			throw new SapsException("Error on validate the file. Missing properties for start Saps Controller.");
 
 		this.properties = properties;
 		this.imageStore = imageStore;
-		this.swiftAPIClient = swiftAPIClient;
 		this.sapsExecutor = Executors.newScheduledThreadPool(1);
-		this.executionMode = properties.containsKey(SapsPropertiesConstants.SAPS_EXECUTION_DEBUG_MODE) && properties
+		this.permanentStorage = createStorageInstance(
+				properties.getProperty(SapsPropertiesConstants.SAPS_PERMANENT_STORAGE_TYPE));
+		this.executionDebugMode = properties.containsKey(SapsPropertiesConstants.SAPS_EXECUTION_DEBUG_MODE) && properties
 				.getProperty(SapsPropertiesConstants.SAPS_EXECUTION_DEBUG_MODE).toLowerCase().equals("true");
 
-		if (this.executionMode && !checkPropertiesDebugMode(properties))
-			throw new SapsException("Error on validate the file. Missing properties for start Saps Controller.");
+		
+	}
 
-		// Creating Swift container
-		this.swiftAPIClient.createContainer(getContainerName());
+	/**
+	 * This function create an instance from declared permanent storage type.
+	 * 
+	 * @param type permanent storage type to be created
+	 * @return permanent storage instance
+	 * @throws SapsException
+	 */
+	private PermanentStorage createStorageInstance(String type) throws SapsException {
+		String lType = type.toLowerCase();
+		if (lType.equals("swift"))
+			return new SwiftPermanentStorage(properties);
+		if (lType.equals("nfs"))
+			return new NfsPermanentStorage();
+
+		throw new SapsException("Failed to recognize type of permanent storage");
 	}
 
 	/**
@@ -94,54 +107,12 @@ public class Archiver {
 			LOGGER.error("Required property " + SapsPropertiesConstants.SAPS_EXPORT_PATH + " was not set");
 			return false;
 		}
-		if (!properties.containsKey(SapsPropertiesConstants.SWIFT_FOLDER_PREFIX)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.SWIFT_FOLDER_PREFIX + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.SWIFT_CONTAINER_NAME)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.SWIFT_CONTAINER_NAME + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.FOGBOW_KEYSTONEV3_PROJECT_ID)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.FOGBOW_KEYSTONEV3_PROJECT_ID + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.FOGBOW_KEYSTONEV3_USER_ID)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.FOGBOW_KEYSTONEV3_USER_ID + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.FOGBOW_KEYSTONEV3_PASSWORD)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.FOGBOW_KEYSTONEV3_PASSWORD + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.FOGBOW_KEYSTONEV3_AUTH_URL)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.FOGBOW_KEYSTONEV3_AUTH_URL + " was not set");
-			return false;
-		}
-		if (!properties.containsKey(SapsPropertiesConstants.FOGBOW_KEYSTONEV3_SWIFT_URL)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.FOGBOW_KEYSTONEV3_SWIFT_URL + " was not set");
+		if (!properties.containsKey(SapsPropertiesConstants.SAPS_PERMANENT_STORAGE_TYPE)) {
+			LOGGER.error("Required property " + SapsPropertiesConstants.SAPS_PERMANENT_STORAGE_TYPE + " was not set");
 			return false;
 		}
 
 		LOGGER.debug("All properties are set");
-		return true;
-	}
-
-	/**
-	 * This function checks if properties for debug mode have been set.
-	 * 
-	 * @param properties saps properties to be check
-	 * @return boolean representation, true (case all properties been set) or false
-	 *         (otherwise)
-	 */
-	private boolean checkPropertiesDebugMode(Properties properties) {
-		if (!properties.containsKey(SapsPropertiesConstants.SWIFT_FOLDER_PREFIX_DEBUG_FAILED_TASKS)) {
-			LOGGER.error("Required property " + SapsPropertiesConstants.SWIFT_FOLDER_PREFIX_DEBUG_FAILED_TASKS
-					+ " was not set (it's necessary when debug mode)");
-			return false;
-		}
-
-		LOGGER.debug("All properties for debug mode are set");
 		return true;
 	}
 
@@ -209,7 +180,8 @@ public class Archiver {
 		for (SapsImage task : archivingTasks) {
 			LOGGER.info("Applying task [" + task.getTaskId() + "] rollback");
 			rollBackArchive(task);
-			deletePendingFilesFromSwift(task);
+			permanentStorage.delete(task);
+			// deletePendingFilesFromSwift(task);
 		}
 	}
 
@@ -239,38 +211,10 @@ public class Archiver {
 		removeTimestampTaskInCatalog(task, "removes task [" + taskId + "] timestamp");
 
 		updateStateInCatalog(task, ImageTaskState.FINISHED, SapsImage.AVAILABLE,
-				"Max archive tries" + MAX_ARCHIVE_TRIES + " reached", SapsImage.NONE_ARREBOL_JOB_ID,
+				"Max archive tries reached", SapsImage.NONE_ARREBOL_JOB_ID,
 				"updates task [" + taskId + "] to failed state");
 
 		addTimestampTaskInCatalog(task, "updates task [" + taskId + "] timestamp");
-	}
-
-	/**
-	 * This function delete pending files from task in Swift.
-	 * 
-	 * @param task task with files information to be deleted
-	 * @return boolean representation, success (true) or failure (false) to delete
-	 *         files
-	 * @throws Exception
-	 */
-	private boolean deletePendingFilesFromSwift(SapsImage task) throws Exception {
-		String containerName = getContainerName();
-		String taskId = task.getTaskId();
-
-		LOGGER.debug("Deleting files from task [" + taskId + "] in Swift [" + containerName + "]");
-
-		String prefix = properties.getProperty(SapsPropertiesConstants.SWIFT_FOLDER_PREFIX) + File.separator + taskId;
-
-		List<String> fileNames = swiftAPIClient.listFilesWithPrefix(containerName, prefix);
-
-		LOGGER.info("Files List: " + fileNames);
-
-		for (String file : fileNames) {
-			LOGGER.debug("Trying to delete file " + file + " from " + containerName);
-			swiftAPIClient.deleteFile(containerName, file);
-		}
-
-		return true;
 	}
 
 	/**
@@ -314,7 +258,7 @@ public class Archiver {
 	 */
 	private void tryTaskArchive(SapsImage task) throws IOException {
 		if (prepareArchive(task)
-				&& archiveSwift(task, properties.getProperty(SapsPropertiesConstants.SWIFT_FOLDER_PREFIX))) {
+				&& permanentStorage.archive(task)) {
 			LOGGER.info("SUCCESS in archiving task [" + task.getTaskId() + "]");
 			finishArchive(task);
 		} else {
@@ -346,53 +290,6 @@ public class Archiver {
 	}
 
 	/**
-	 * This function tries to archive a task trying each folder in order
-	 * (inputdownloading -> preprocessing -> processing).
-	 * 
-	 * @param task task to be archived
-	 * @return boolean representation, success (true) or failure (false) in to
-	 *         archive the three folders.
-	 */
-	private boolean archiveSwift(SapsImage task, String swiftExports) {
-		String taskId = task.getTaskId();
-
-		LOGGER.info("Attempting to archive task [" + taskId + "] with a maximum of " + MAX_ARCHIVE_TRIES
-				+ " archiving attempts for each folder (inputdownloading, preprocessing, processing)");
-
-		String sapsExports = properties.getProperty(SapsPropertiesConstants.SAPS_EXPORT_PATH);
-
-		String inputdownloadingLocalDir = sapsExports + File.separator + taskId + File.separator + "inputdownloading";
-		String inputdownloadingSwiftDir = swiftExports + File.separator + taskId + File.separator + "inputdownloading";
-
-		String preprocessingLocalDir = sapsExports + File.separator + taskId + File.separator + "preprocessing";
-		String preprocessingSwiftDir = swiftExports + File.separator + taskId + File.separator + "preprocessing";
-
-		String processingLocalDir = sapsExports + File.separator + taskId + File.separator + "processing";
-		String processingSwiftDir = swiftExports + File.separator + taskId + File.separator + "processing";
-
-		LOGGER.info("Inputdownloading local folder: " + inputdownloadingLocalDir);
-		LOGGER.info("Inputdownloading swift folder: " + inputdownloadingSwiftDir);
-		boolean inputdownloadingSentSuccess = archive(task, inputdownloadingLocalDir, inputdownloadingSwiftDir);
-
-		LOGGER.info("Preprocessing local folder: " + preprocessingLocalDir);
-		LOGGER.info("Preprocessing swift folder: " + preprocessingSwiftDir);
-		boolean preprocessingSentSuccess = inputdownloadingSentSuccess
-				&& archive(task, preprocessingLocalDir, preprocessingSwiftDir);
-
-		LOGGER.info("Processing local folder: " + processingLocalDir);
-		LOGGER.info("Processing swift folder: " + processingSwiftDir);
-		boolean processingSentSuccess = preprocessingSentSuccess
-				&& archive(task, processingLocalDir, processingSwiftDir);
-
-		LOGGER.info("Archive process result of task [" + task.getTaskId() + ":\nInputdownloading phase: "
-				+ (inputdownloadingSentSuccess ? "Sucesso" : "Falha") + "\n" + "Preprocessing phase: "
-				+ (preprocessingSentSuccess ? "Sucesso" : "Falha") + "\n" + "Processing phase: "
-				+ (processingSentSuccess ? "Sucesso" : "Falha"));
-
-		return inputdownloadingSentSuccess && preprocessingSentSuccess && processingSentSuccess;
-	}
-
-	/**
 	 * This function finishes a success task.
 	 * 
 	 * @param task task to be finished
@@ -416,7 +313,7 @@ public class Archiver {
 		String taskId = task.getTaskId();
 
 		updateStateInCatalog(task, ImageTaskState.FAILED, SapsImage.AVAILABLE,
-				"Max archive tries" + MAX_ARCHIVE_TRIES + " reached", SapsImage.NONE_ARREBOL_JOB_ID,
+				"Max archive tries reached", SapsImage.NONE_ARREBOL_JOB_ID,
 				"updates task [" + taskId + "] to failed state");
 		addTimestampTaskInCatalog(task, "updates task [" + taskId + "] timestamp");
 	}
@@ -434,9 +331,8 @@ public class Archiver {
 
 		File taskDir = new File(taskDirPath);
 		if (taskDir.exists() && taskDir.isDirectory()) {
-			if (this.executionMode && task.getState().equals(ImageTaskState.FAILED))
-				archiveSwift(task,
-						properties.getProperty(SapsPropertiesConstants.SWIFT_FOLDER_PREFIX_DEBUG_FAILED_TASKS));
+			if (this.executionDebugMode && task.getState().equals(ImageTaskState.FAILED))
+				permanentStorage.archive(task);
 			try {
 				FileUtils.deleteDirectory(taskDir);
 			} catch (IOException e) {
@@ -487,82 +383,5 @@ public class Archiver {
 	 */
 	private void removeTimestampTaskInCatalog(SapsImage task, String message) {
 		CatalogUtils.removeTimestampTask(imageStore, task, message);
-	}
-
-	/**
-	 * This function tries to archive a task folder in Swift.
-	 * 
-	 * @param task     task in archive process
-	 * @param localDir task folder to be archived
-	 * @param swiftDir directory swift to archive new data
-	 * @return boolean representation, success (true) or failure (false) to archive
-	 */
-	private boolean archive(SapsImage task, String localDir, String swiftDir) {
-		LOGGER.info("Trying to archive task [" + task.getTaskId() + "] " + localDir + " folder with a maximum of "
-				+ MAX_ARCHIVE_TRIES + " archiving attempts");
-
-		File localFileDir = new File(localDir);
-
-		if (!localFileDir.exists() || !localFileDir.isDirectory()) {
-			LOGGER.error("Failed to archive task [" + task.getTaskId() + "]. " + localDir
-					+ " folder isn't directory or not exists");
-			return false;
-		}
-
-		for (int itry = 0; itry < MAX_ARCHIVE_TRIES; itry++) {
-			if (uploadFilesToSwift(task, localFileDir, swiftDir))
-				return true;
-		}
-
-		return false;
-	}
-
-	/**
-	 * This function tries upload task folder files to Swift.
-	 * 
-	 * @param task     task in archive process
-	 * @param localDir task folder to be archived
-	 * @param swiftDir directory swift to archive new data
-	 * @return boolean representation, success (true) or failure (false) to archive
-	 */
-	private boolean uploadFilesToSwift(SapsImage task, File localDir, String swiftDir) {
-		LOGGER.info("Trying to archive task [" + task.getTaskId() + "] " + localDir + " folder for swift");
-		for (File actualFile : localDir.listFiles()) {
-			if (!uploadFileToSwift(actualFile, swiftDir)) {
-				LOGGER.info("Failure in archiving file [" + actualFile.getAbsolutePath() + "]");
-				return false;
-			}
-		}
-
-		LOGGER.info("Upload to swift succsessfully done");
-		return true;
-	}
-
-	/**
-	 * This function tries upload a task folder file to Swift.
-	 * 
-	 * @param actualFile file to be uploaded
-	 * @param swiftDir   directory swift to archive
-	 * @return boolean representation, success (true) or failure (false) to archive
-	 */
-	private boolean uploadFileToSwift(File actualFile, String swiftDir) {
-		String containerName = getContainerName();
-		LOGGER.info("Trying to archive file [" + actualFile.getAbsolutePath() + "] for swift container ["
-				+ containerName + "] with a maximum of " + MAX_SWIFT_UPLOAD_TRIES + " uploading attempts");
-
-		for (int itry = 0; itry < MAX_SWIFT_UPLOAD_TRIES; itry++) {
-			try {
-				swiftAPIClient.uploadFile(containerName, actualFile, swiftDir);
-				return true;
-			} catch (Exception e) {
-				LOGGER.error("Error while uploading file " + actualFile.getAbsolutePath() + " to swift", e);
-			}
-		}
-
-		return false;
-	}
-
-	private String getContainerName() {
-		return properties.getProperty(SapsPropertiesConstants.SWIFT_CONTAINER_NAME);
 	}
 }
