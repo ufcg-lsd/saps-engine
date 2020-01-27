7c6cf816a5d2f78627d1467320b6dd59323cec85
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-01-27 15:59:46 -0300

    Create NFS permanent storage class

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/NfsPermanentStorage.java
@@ -0,0 +1,19 @@
+package org.fogbowcloud.saps.engine.core.archiver.storage;
+
+import org.fogbowcloud.saps.engine.core.model.SapsImage;
+
+public class NfsPermanentStorage implements PermanentStorage {
+
+	@Override
+	public boolean archive(SapsImage task) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(SapsImage task) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
