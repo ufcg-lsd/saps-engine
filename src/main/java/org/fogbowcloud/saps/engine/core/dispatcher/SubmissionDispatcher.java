a75fa92a739d29963822b862294fe91ebba11399
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-01-27 11:48:44 -0300

    Implemented method for get digest from Docker image

--- src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/SubmissionDispatcher.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/SubmissionDispatcher.java
@@ -346,9 +346,32 @@ public class SubmissionDispatcher {
 	 * @param imageDockerInfo image docker information
 	 * @return immutable identifier that match with repository and tag passed
 	 */
-	// TODO implement method
 	private String getDigest(ExecutionScriptTag imageDockerInfo) {
-		return null;
+
+		String dockerRepository = imageDockerInfo.getDockerRepository();
+		String dockerTag = imageDockerInfo.getDockerTag();
+
+		String result = null;
+
+		try {
+			Process builder = new ProcessBuilder("bash", "./scripts/get_digest.sh", dockerRepository, dockerTag)
+					.start();
+
+			LOGGER.debug("Waiting for the process for execute command: " + builder.toString());
+			builder.waitFor();
+
+			if (builder.exitValue() != 0)
+				throw new Exception("Process output exit code: " + builder.exitValue());
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(builder.getInputStream()));
+
+			result = reader.readLine();
+		} catch (Exception e) {
+			LOGGER.error("Error while trying get digest from Docker image [" + dockerRepository + "] with tag ["
+					+ dockerTag + "].", e);
+		}
+
+		return result;
 	}
 
 	/**
