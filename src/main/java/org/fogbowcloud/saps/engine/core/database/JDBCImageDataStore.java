fb4757f3fc9a13e50e880977944dcad72c2d1d8e
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-01-28 16:07:29 -0300

    Unnecessary imports removed

--- src/main/java/org/fogbowcloud/saps/engine/core/database/JDBCImageDataStore.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/database/JDBCImageDataStore.java
@@ -8,12 +8,8 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.log4j.Logger;
 import org.fogbowcloud.saps.engine.core.dispatcher.notifier.Ward;
