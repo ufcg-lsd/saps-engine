0ae242fe64adc9adc76ec53e6edc20d23a1a083c
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-04 12:46:14 -0300

    Reformat code

--- src/main/java/org/fogbowcloud/saps/engine/core/database/Catalog.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/database/Catalog.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+
 import org.fogbowcloud.saps.engine.core.dispatcher.notifier.Ward;
 import org.fogbowcloud.saps.engine.core.model.SapsImage;
 import org.fogbowcloud.saps.engine.core.model.SapsUser;
@@ -11,39 +12,39 @@ import org.fogbowcloud.saps.engine.core.model.enums.ImageTaskState;
 
 public interface Catalog {
 
-	int UNLIMITED = -1;
+    int UNLIMITED = -1;
 
-	SapsImage addTask(String taskId, String dataset, String region, Date date, int priority, String user,
-					  String inputdownloadingPhaseTag, String digestInputdownloading, String preprocessingPhaseTag,
-					  String digestPreprocessing, String processingPhaseTag, String digestProcessing) throws SQLException;
+    SapsImage addTask(String taskId, String dataset, String region, Date date, int priority, String user,
+                      String inputdownloadingPhaseTag, String digestInputdownloading, String preprocessingPhaseTag,
+                      String digestPreprocessing, String processingPhaseTag, String digestProcessing) throws SQLException;
 
-	void addStateStamp(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
+    void addStateStamp(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
 
-	void addUser(String userEmail, String userName, String userPass, boolean userState, boolean userNotify,
-			boolean adminRole) throws SQLException;
+    void addUser(String userEmail, String userName, String userPass, boolean userState, boolean userNotify,
+                 boolean adminRole) throws SQLException;
 
-	void addUserNotification(String submissionId, String taskId, String userEmail) throws SQLException;
+    void addUserNotification(String submissionId, String taskId, String userEmail) throws SQLException;
 
-	List<Ward> getUsersToNotify() throws SQLException;
+    List<Ward> getUsersToNotify() throws SQLException;
 
-	void updateImageTask(SapsImage imageTask) throws SQLException;
+    void updateImageTask(SapsImage imageTask) throws SQLException;
 
-	boolean isUserNotifiable(String userEmail) throws SQLException;
+    boolean isUserNotifiable(String userEmail) throws SQLException;
 
-	List<SapsImage> getAllTasks() throws SQLException;
+    List<SapsImage> getAllTasks() throws SQLException;
 
-	List<SapsImage> getTasksInProcessingStates() throws SQLException;
+    List<SapsImage> getTasksInProcessingStates() throws SQLException;
 
-	List<SapsImage> getTasksByState(ImageTaskState state, int limit) throws SQLException;
+    List<SapsImage> getTasksByState(ImageTaskState state, int limit) throws SQLException;
 
-	SapsImage getTaskById(String taskId) throws SQLException;
+    SapsImage getTaskById(String taskId) throws SQLException;
 
-	SapsUser getUserByEmail(String userEmail) throws SQLException;
+    SapsUser getUserByEmail(String userEmail) throws SQLException;
 
-	void removeNotification(String submissionId, String taskId, String userEmail) throws SQLException;
+    void removeNotification(String submissionId, String taskId, String userEmail) throws SQLException;
 
-	void removeStateStamp(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
+    void removeStateStamp(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
 
-	List<SapsImage> getSuccessfullyProcessedTasks(String region, Date initDate, Date endDate, String inputGathering,
-												  String inputPreprocessing, String algorithmExecution) throws SQLException;
+    List<SapsImage> getSuccessfullyProcessedTasks(String region, Date initDate, Date endDate, String inputGathering,
+                                                  String inputPreprocessing, String algorithmExecution) throws SQLException;
 }
