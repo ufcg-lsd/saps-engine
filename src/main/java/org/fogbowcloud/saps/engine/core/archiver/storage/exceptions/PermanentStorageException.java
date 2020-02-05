f5ebb32168a28b218734b1b339ed4e18d0f45991
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-05 08:52:50 -0300

    Exception class created to handle errors regarding the permanent storage interface

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/exceptions/PermanentStorageException.java
@@ -0,0 +1,10 @@
+package org.fogbowcloud.saps.engine.core.archiver.storage.exceptions;
+
+public class PermanentStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = -2520888793776997437L;
+
+    public PermanentStorageException(String msg){
+        super(msg);
+    }
+}
