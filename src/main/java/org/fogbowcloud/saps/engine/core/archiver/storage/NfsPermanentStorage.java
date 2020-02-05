c85bbdbf42db4778262cc2c87ce9e5da1898d1e5
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-05 09:26:18 -0300

    Release exceptions added to methods that implement the permanent storage interface

--- src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/NfsPermanentStorage.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/NfsPermanentStorage.java
@@ -1,17 +1,18 @@
 package org.fogbowcloud.saps.engine.core.archiver.storage;
 
+import org.fogbowcloud.saps.engine.core.archiver.storage.exceptions.PermanentStorageException;
 import org.fogbowcloud.saps.engine.core.model.SapsImage;
 
 public class NfsPermanentStorage implements PermanentStorage {
 
 	@Override
-	public boolean archive(SapsImage task) {
+	public boolean archive(SapsImage task) throws PermanentStorageException {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public boolean delete(SapsImage task) throws Exception {
+	public boolean delete(SapsImage task) throws PermanentStorageException {
 		// TODO Auto-generated method stub
 		return false;
 	}
