8dda121564e40bab8a082bd2de87d42a81d4ab8a
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-05 10:07:13 -0300

    Release exception changed to CatalogException because SQLException refers to JDBCCatalog implementation

--- src/main/java/org/fogbowcloud/saps/engine/core/catalog/Catalog.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/catalog/Catalog.java
@@ -5,6 +5,7 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
+import org.fogbowcloud.saps.engine.core.catalog.exceptions.CatalogException;
 import org.fogbowcloud.saps.engine.core.catalog.exceptions.TaskNotFoundException;
 import org.fogbowcloud.saps.engine.core.catalog.exceptions.UserNotFoundException;
 import org.fogbowcloud.saps.engine.core.dispatcher.notifier.Ward;
@@ -18,35 +19,35 @@ public interface Catalog {
 
     SapsImage addTask(String taskId, String dataset, String region, Date date, int priority, String user,
                       String inputdownloadingPhaseTag, String digestInputdownloading, String preprocessingPhaseTag,
-                      String digestPreprocessing, String processingPhaseTag, String digestProcessing) throws SQLException;
+                      String digestPreprocessing, String processingPhaseTag, String digestProcessing) throws CatalogException;
 
-    void addStateChangeTime(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
+    void addStateChangeTime(String taskId, ImageTaskState state, Timestamp timestamp) throws CatalogException;
 
     void addUser(String userEmail, String userName, String userPass, boolean userState, boolean userNotify,
-                 boolean adminRole) throws SQLException;
+                 boolean adminRole) throws CatalogException;
 
-    void addUserNotification(String submissionId, String taskId, String userEmail) throws SQLException;
+    void addUserNotification(String submissionId, String taskId, String userEmail) throws CatalogException;
 
-    List<Ward> getUsersToNotify() throws SQLException;
+    List<Ward> getUsersToNotify() throws CatalogException;
 
-    void updateImageTask(SapsImage imageTask) throws SQLException;
+    void updateImageTask(SapsImage imageTask) throws CatalogException;
 
-    boolean isUserNotifiable(String userEmail) throws SQLException;
+    boolean isUserNotifiable(String userEmail) throws CatalogException;
 
-    List<SapsImage> getAllTasks() throws SQLException;
+    List<SapsImage> getAllTasks() throws CatalogException;
 
-    List<SapsImage> getTasksInProcessingStates() throws SQLException;
+    List<SapsImage> getTasksInProcessingStates() throws CatalogException;
 
-    List<SapsImage> getTasksByState(ImageTaskState state, int limit) throws SQLException;
+    List<SapsImage> getTasksByState(ImageTaskState state, int limit) throws CatalogException;
 
-    SapsImage getTaskById(String taskId) throws SQLException, TaskNotFoundException;
+    SapsImage getTaskById(String taskId) throws CatalogException, TaskNotFoundException;
 
-    SapsUser getUserByEmail(String userEmail) throws SQLException, UserNotFoundException;
+    SapsUser getUserByEmail(String userEmail) throws CatalogException, UserNotFoundException;
 
-    void removeNotification(String submissionId, String taskId, String userEmail) throws SQLException;
+    void removeNotification(String submissionId, String taskId, String userEmail) throws CatalogException;
 
-    void removeStateChangeTime(String taskId, ImageTaskState state, Timestamp timestamp) throws SQLException;
+    void removeStateChangeTime(String taskId, ImageTaskState state, Timestamp timestamp) throws CatalogException;
 
     List<SapsImage> getSuccessfullyProcessedTasks(String region, Date initDate, Date endDate, String inputGathering,
-                                                  String inputPreprocessing, String algorithmExecution) throws SQLException;
+                                                  String inputPreprocessing, String algorithmExecution) throws CatalogException;
 }
