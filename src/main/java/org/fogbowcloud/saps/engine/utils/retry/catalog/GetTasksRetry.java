4192e3a9249d0c1a2fecbd3d101fabb103a9a500
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-10 11:27:00 -0300

    State array moved to declare it in the constructor and use it in the run method

--- src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/GetTasksRetry.java
+++ src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/GetTasksRetry.java
@@ -9,18 +9,17 @@ import org.fogbowcloud.saps.engine.core.model.enums.ImageTaskState;
 public class GetTasksRetry implements CatalogRetry<List<SapsImage>>{
 
 	private Catalog imageStore;
-	private ImageTaskState state;
+	private ImageTaskState[] states;
 	private Integer limit;
 	
 	public GetTasksRetry(Catalog imageStore, ImageTaskState state, Integer limit) {
 		this.imageStore = imageStore;
-		this.state = state;
+		this.states = new ImageTaskState[]{state};
 		this.limit = limit;
 	}
 	
 	@Override
 	public List<SapsImage> run() {
-		ImageTaskState[] states = {state};
 		return imageStore.getTasksByState(limit, states);
 	}
 
