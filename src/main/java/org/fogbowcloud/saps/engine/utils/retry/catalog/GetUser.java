63bdffab61906ce124c2045b3f9fc9458edf7577
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-10 11:00:54 -0300

    Log message added for report the problem

--- src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/GetUser.java
+++ src/main/java/org/fogbowcloud/saps/engine/utils/retry/catalog/GetUser.java
@@ -1,13 +1,16 @@
 package org.fogbowcloud.saps.engine.utils.retry.catalog;
 
+import org.apache.log4j.Logger;
 import org.fogbowcloud.saps.engine.core.catalog.Catalog;
 import org.fogbowcloud.saps.engine.core.catalog.exceptions.CatalogException;
+import org.fogbowcloud.saps.engine.core.catalog.exceptions.UserNotFoundException;
 import org.fogbowcloud.saps.engine.core.model.SapsUser;
 
 public class GetUser implements CatalogRetry<SapsUser> {
 
 	private Catalog imageStore;
 	private String userEmail;
+	public static final Logger LOGGER = Logger.getLogger(GetUser.class);
 
 	public GetUser(Catalog imageStore, String userEmail) {
 		this.imageStore = imageStore;
@@ -18,8 +21,8 @@ public class GetUser implements CatalogRetry<SapsUser> {
 	public SapsUser run() {
 		try {
 			return imageStore.getUserByEmail(userEmail);
-		} catch (CatalogException e) {
-			// nothing
+		} catch (CatalogException | UserNotFoundException e) {
+			LOGGER.error("Error while gets user by email.", e);
 		}
 		return null;
 	}
