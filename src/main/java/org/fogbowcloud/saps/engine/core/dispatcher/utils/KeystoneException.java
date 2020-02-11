eace95388c0af64f9b4f256cd682c240875c8b9b
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-02-11 11:41:33 -0300

    Created Keystone Exception

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/utils/KeystoneException.java
@@ -0,0 +1,8 @@
+package org.fogbowcloud.saps.engine.core.dispatcher.utils;
+
+public class KeystoneException extends RuntimeException {
+
+    public KeystoneException(String s) {
+        super(s);
+    }
+}
