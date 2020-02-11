9598be6af48553aed7ed9b1cef658b312ebc1b53
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-02-11 11:49:49 -0300

    Created unit test to createAccessId method from KeystoneV3IdentityPluginTest

---
+++ src/test/java/org/fogbowcloud/saps/engine/core/dispatcher/utils/KeystoneV3IdentityPluginTest.java
@@ -0,0 +1,40 @@
+package org.fogbowcloud.saps.engine.core.dispatcher.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyStoreException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import org.fogbowcloud.saps.engine.utils.SapsPropertiesConstants;
+import org.junit.Test;
+
+public class KeystoneV3IdentityPluginTest {
+
+    private static final String CONFIG_FILE_PATH = "src/test/resources/dispatcher-test.conf";
+
+    @Test
+    public void createAccessId() throws IOException, KeyStoreException {
+        Properties properties = new Properties();
+        FileInputStream input = new FileInputStream(CONFIG_FILE_PATH);
+        properties.load(input);
+        KeystoneV3IdentityPlugin keystone = new KeystoneV3IdentityPlugin();
+
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put(KeystoneV3IdentityPlugin.AUTH_URL,
+            properties.getProperty(SapsPropertiesConstants.SWIFT_AUTH_URL));
+        credentials.put(KeystoneV3IdentityPlugin.PROJECT_ID,
+            properties.getProperty(SapsPropertiesConstants.SWIFT_PROJECT_ID));
+        credentials.put(KeystoneV3IdentityPlugin.USER_ID,
+            properties.getProperty(SapsPropertiesConstants.SWIFT_USER_ID));
+        credentials.put(KeystoneV3IdentityPlugin.PASSWORD,
+            properties.getProperty(SapsPropertiesConstants.SWIFT_PASSWORD));
+
+        String accessId = keystone.createAccessId(credentials);
+        assertNotNull(accessId);
+        assertFalse(accessId.isEmpty());
+    }
+}
