825c917fba9a888e28ad17988aa77285b8a3ba15
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-02-18 10:14:33 -0300

    Exception class of the JDBC implementation recreated for use in the JDBC utility class

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/catalog/jdbc/exceptions/JDBCCatalogException.java
@@ -0,0 +1,10 @@
+package org.fogbowcloud.saps.engine.core.catalog.jdbc.exceptions;
+
+public class JDBCCatalogException extends Exception {
+
+    private static final long serialVersionUID = -2520888793776997437L;
+
+    public JDBCCatalogException(String msg) {
+        super(msg);
+    }
+}
