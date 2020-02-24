97a13f89908825c8678856b6c94c72ea2257e8bd
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-02-24 16:05:19 -0300

    Added ArrebolConnectException

---
+++ src/main/java/org/fogbowcloud/saps/engine/core/scheduler/arrebol/exceptions/ArrebolConnectException.java
@@ -0,0 +1,12 @@
+package org.fogbowcloud.saps.engine.core.scheduler.arrebol.exceptions;
+
+public class ArrebolConnectException extends Exception {
+
+    public ArrebolConnectException(String message) {
+        super(message);
+    }
+
+    public ArrebolConnectException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
