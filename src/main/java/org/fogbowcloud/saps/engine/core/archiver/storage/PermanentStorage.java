41e315615cd50d112ab37f578a0528a284aa9e76
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-03-04 10:59:06 -0300

    Update code for equal PermanentStorage interface from feature/swift-generate-link branch

--- src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/PermanentStorage.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/PermanentStorage.java
@@ -1,16 +1,15 @@
 package org.fogbowcloud.saps.engine.core.archiver.storage;
 
+import java.util.List;
 import org.fogbowcloud.saps.engine.core.archiver.storage.exceptions.PermanentStorageException;
 import org.fogbowcloud.saps.engine.core.model.SapsImage;
-
-import java.util.List;
 
 public interface PermanentStorage {
 
 	/**
 	 * This function tries to archive a task trying each dirs in order
 	 * (inputdownloading -> preprocessing -> processing).
-	 * 
+	 *
 	 * @param task task to be archived
 	 * @return boolean representation, success (true) or failure (false) in to
 	 *         archive the three dirs.
@@ -19,7 +18,7 @@ public interface PermanentStorage {
 
 	/**
 	 * This function delete all files from task in Permanent Storage.
-	 * 
+	 *
 	 * @param task task with files information to be deleted
 	 * @return boolean representation, success (true) or failure (false) to delete
 	 *         files
@@ -28,4 +27,5 @@ public interface PermanentStorage {
 	boolean delete(SapsImage task) throws PermanentStorageException;
 
 	List<AccessLink> generateAccessLinks(String taskId) throws PermanentStorageException;
+
 }
