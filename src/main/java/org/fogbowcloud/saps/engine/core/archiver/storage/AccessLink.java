bc2cb3cb124c58a47978bc454c0aac6bf635c562
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-03-05 16:29:14 -0300

    Added fields checkers for constructor of Access Link

--- src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/AccessLink.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/archiver/storage/AccessLink.java
@@ -1,10 +1,19 @@
 package org.fogbowcloud.saps.engine.core.archiver.storage;
 
+import java.util.Objects;
+
 public class AccessLink {
+
     private final String name;
     private final String url;
 
     public AccessLink(String name, String url) {
+        if(Objects.isNull(name) || name.isEmpty()) {
+            throw new IllegalArgumentException("The name field of Access Link may be not empty or null.");
+        }
+        if(Objects.isNull(url) || url.trim().isEmpty()) {
+            throw new IllegalArgumentException("The url field of Access Link may be not empty or null.");
+        }
         this.name = name;
         this.url = url;
     }
