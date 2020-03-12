6ea512a2409e7d390c8919f28b451f508b8b171e
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-03-12 16:44:39 -0300

    Added permanent storage creator method and updated ProcessedImagesEmailBuilder class constructor

--- src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/restlet/resource/ProcessedImagesResource.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/restlet/resource/ProcessedImagesResource.java
@@ -2,7 +2,13 @@ package org.fogbowcloud.saps.engine.core.dispatcher.restlet.resource;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.log4j.Logger;
+import org.fogbowcloud.saps.engine.core.archiver.storage.PermanentStorage;
+import org.fogbowcloud.saps.engine.core.archiver.storage.PermanentStorageType;
+import org.fogbowcloud.saps.engine.core.archiver.storage.exceptions.PermanentStorageException;
+import org.fogbowcloud.saps.engine.core.archiver.storage.nfs.NfsPermanentStorage;
+import org.fogbowcloud.saps.engine.core.archiver.storage.swift.SwiftPermanentStorage;
 import org.fogbowcloud.saps.engine.core.dispatcher.email.ProcessedImagesEmailBuilder;
+import org.fogbowcloud.saps.engine.utils.SapsPropertiesConstants;
 import org.restlet.data.Form;
 import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
@@ -10,34 +16,54 @@ import org.restlet.representation.StringRepresentation;
 import org.restlet.resource.Post;
 import org.restlet.resource.ResourceException;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class ProcessedImagesResource extends BaseResource {
 
-	public static final Logger LOGGER = Logger.getLogger(ProcessedImagesResource.class);
+    private static final Logger LOGGER = Logger.getLogger(ProcessedImagesResource.class);
 
-	private static final String REQUEST_ATTR_PROCESSED_IMAGES = "images_id[]";
+    private static final String REQUEST_ATTR_PROCESSED_IMAGES = "images_id[]";
 
-	@Post
-	public Representation sendProcessedImagesToEmail(Representation representation) {
-		Form form = new Form(representation);
+    @Post
+    public Representation sendProcessedImagesToEmail(Representation representation) {
+        Form form = new Form(representation);
 
-		String userEmail = form.getFirstValue(UserResource.REQUEST_ATTR_USER_EMAIL, true);
-		String userPass = form.getFirstValue(UserResource.REQUEST_ATTR_USERPASS, true);
-		if (!authenticateUser(userEmail, userPass) || userEmail.equals("anonymous")) {
-			throw new ResourceException(HttpStatus.SC_UNAUTHORIZED);
-		}
+        String userEmail = form.getFirstValue(UserResource.REQUEST_ATTR_USER_EMAIL, true);
+        String userPass = form.getFirstValue(UserResource.REQUEST_ATTR_USERPASS, true);
+        if (!authenticateUser(userEmail, userPass) || userEmail.equals("anonymous"))
+            throw new ResourceException(HttpStatus.SC_UNAUTHORIZED);
 
-		String[] imageIds = form.getValuesArray(REQUEST_ATTR_PROCESSED_IMAGES, true);
-		Properties properties = application.getProperties();
+        String[] imageIds = form.getValuesArray(REQUEST_ATTR_PROCESSED_IMAGES, true);
+        Properties properties = application.getProperties();
 
-		ProcessedImagesEmailBuilder emailBuilder = new ProcessedImagesEmailBuilder(application, properties, userEmail,
-				Arrays.asList(imageIds));
+        try {
+            PermanentStorage permanentStorage = createPermanentStorage(properties);
 
-		Thread thread = new Thread(emailBuilder);
-		thread.start();
+            ProcessedImagesEmailBuilder emailBuilder = new ProcessedImagesEmailBuilder(application, permanentStorage,
+					properties, userEmail, Arrays.asList(imageIds));
 
-		return new StringRepresentation("Email será enviado em breve.", MediaType.TEXT_PLAIN);
-	}
+            Thread thread = new Thread(emailBuilder);
+            thread.start();
+
+            return new StringRepresentation("Email will be sent soon.", MediaType.TEXT_PLAIN);
+        } catch (PermanentStorageException | IOException e) {
+            LOGGER.error("Error while create permanent storage", e);
+            return new StringRepresentation("An error occurred while sending the email, please try again later.", MediaType.TEXT_PLAIN);
+        }
+
+    }
+
+    private PermanentStorage createPermanentStorage(Properties properties)
+            throws PermanentStorageException, IOException {
+        String permanentStorageType = properties
+                .getProperty(SapsPropertiesConstants.SAPS_PERMANENT_STORAGE_TYPE);
+        if (PermanentStorageType.SWIFT.toString().equalsIgnoreCase(permanentStorageType)) {
+            return new SwiftPermanentStorage(properties);
+        } else if (PermanentStorageType.NFS.toString().equalsIgnoreCase(permanentStorageType)) {
+            return new NfsPermanentStorage(properties);
+        }
+        throw new IOException("Failed to recognize type of permanent storage");
+    }
 }
