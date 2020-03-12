750d7ec68460d1d31438c1a8f70b92275cd20720
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-03-12 16:26:47 -0300

    Updated Identity Token properties fields

--- src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/email/keystone/IdentityToken.java
+++ src/main/java/org/fogbowcloud/saps/engine/core/dispatcher/email/keystone/IdentityToken.java
@@ -1,25 +1,26 @@
 package org.fogbowcloud.saps.engine.core.dispatcher.email.keystone;
 
-import java.time.Instant;
-
 public class IdentityToken {
 
     private final String accessId;
-    private final Instant createdAt;
-    private final Long duration;
+    private final String issuedAt;
+    private final String expiresAt;
 
     public IdentityToken(String accessId, String issuedAt, String expiresAt) {
         this.accessId = accessId;
-        this.createdAt = Instant.now();
-        //TODO Handler null values
-        this.duration = Instant.parse(expiresAt).getEpochSecond() - Instant.parse(issuedAt).getEpochSecond();
+        this.issuedAt = issuedAt;
+        this.expiresAt = expiresAt;
     }
 
     public String getAccessId() {
         return accessId;
     }
 
-    public boolean isExpired() {
-        return Instant.now().isAfter(createdAt.plusMillis(duration));
+    public String getIssuedAt() {
+        return issuedAt;
+    }
+
+    public String getExpiresAt() {
+        return expiresAt;
     }
 }
