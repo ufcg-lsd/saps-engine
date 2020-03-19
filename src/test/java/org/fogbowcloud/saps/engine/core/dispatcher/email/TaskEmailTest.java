56668021a1f238cd18d3e5ab00bdf8fed68c6017
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-03-19 18:08:17 -0300

    Updated test for to use unique simple constructor

--- src/test/java/org/fogbowcloud/saps/engine/core/dispatcher/email/TaskEmailTest.java
+++ src/test/java/org/fogbowcloud/saps/engine/core/dispatcher/email/TaskEmailTest.java
@@ -2,10 +2,13 @@ package org.fogbowcloud.saps.engine.core.dispatcher.email;
 
 import com.google.gson.Gson;
 import org.fogbowcloud.saps.engine.core.archiver.storage.AccessLink;
+import org.fogbowcloud.saps.engine.core.model.SapsImage;
+import org.fogbowcloud.saps.engine.core.model.enums.ImageTaskState;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Timestamp;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.LinkedList;
@@ -14,6 +17,7 @@ import java.util.List;
 public class TaskEmailTest {
 
     private Gson jsonUtil;
+    private SapsImage task;
     private static final String MOCK_ID = "task-email-id";
     private static final String MOCK_IMAGE_REGION = "task-image-region";
     private static final String MOCK_IMAGE_COLLECTION_NAME = "image-collection-name";
@@ -21,15 +25,17 @@ public class TaskEmailTest {
     private static final List<AccessLink> MOCK_ACCESS_LINKS = new LinkedList<>();
 
     @Before
-    public void setUp(){
+    public void setUp() {
         jsonUtil = new Gson();
+        task = new SapsImage(MOCK_ID, "", MOCK_IMAGE_REGION, MOCK_IMAGE_DATE, ImageTaskState.ARCHIVED,
+                SapsImage.NONE_ARREBOL_JOB_ID, SapsImage.NONE_FEDERATION_MEMBER, 0, "", "", "", "", "",
+                "", "", new Timestamp(1), new Timestamp(1), "", "");
     }
 
     @Test
     //TODO upgrade this test
-    public void testToJson(){
-        TaskEmail taskEmail = new TaskEmail(MOCK_ID, MOCK_IMAGE_REGION,
-                MOCK_IMAGE_COLLECTION_NAME, MOCK_IMAGE_DATE, MOCK_ACCESS_LINKS);
+    public void testToJson() {
+        TaskEmail taskEmail = new TaskEmail(task, MOCK_ACCESS_LINKS);
 
         String taskEmailInString = jsonUtil.toJson(taskEmail);
         Assert.assertNotNull(taskEmailInString);
